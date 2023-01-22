d376b7d417353bea23981bc7551608cb2476c7f6
Cowboy <cbrobak79@gmail.com>
2023-01-22 18:40:54 -0500

    Full assignment

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :orders
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :phone,  presence: true
@@ -7,8 +8,8 @@ class Customer < ApplicationRecord
     validates :email, presence: true
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
    
-   
     def full_name
       "#{first_name} #{last_name}"
     end
+    
    end
